3a9fd1c22ebf810dabed44df1719e4c670952ca2
zabi_1112 <66674164+Zabi1112@users.noreply.github.com>
2026-05-26 22:25:56 +0500

    vercel json added

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,24 +1,54 @@
-const { Sequelize } = require("sequelize");
+// For Local PostgreSQL Database
+// const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  String(process.env.DB_PASSWORD),
-  {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    dialect: "postgres",
-    logging: false,
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   String(process.env.DB_PASSWORD),
+//   {
+//     host: process.env.DB_HOST,
+//     port: Number(process.env.DB_PORT),
+//     dialect: "postgres",
+//     logging: false,
+//   }
+// );
+
+// const connectDB = async () => {
+//   try {
+//     console.log("DB_USER:", process.env.DB_USER);
+//     console.log("DB_NAME:", process.env.DB_NAME);
+
+//     await sequelize.authenticate();
+//     console.log("PostgreSQL Connected");
+//   } catch (error) {
+//     console.error("PostgreSQL Error:", error.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = { sequelize, connectDB };
+
+
+// For SupaBase PostgreSQL Database
+
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 const connectDB = async () => {
   try {
-    console.log("DB_USER:", process.env.DB_USER);
-    console.log("DB_NAME:", process.env.DB_NAME);
-
     await sequelize.authenticate();
-    console.log("PostgreSQL Connected");
+    console.log("Supabase PostgreSQL Connected");
   } catch (error) {
     console.error("PostgreSQL Error:", error.message);
     process.exit(1);
